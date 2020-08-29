7bfb415ed6e4e9dbe35bfd1dd89ea76e5e56c320
LoulouCrypto <68774649+LoulouCrypto@users.noreply.github.com>
2020-08-29 17:26:40 +0200

    Update update-multi-2-1-4.sh

--- update-multi-2-1-4.sh
+++ update-multi-2-1-4.sh
@@ -6,7 +6,7 @@ COIN_PATH='/usr/local/bin/'
 #64 bit only
 COIN_TGZ='https://github.com/fdreserve/fdr-blockchain/releases/download/2.1.4/fdr-v2.1.4-linux64.tar.gz'
 COIN_PATHPART='fdr-v2.1.4-linux/bin'
-BOOTSTRAP_TGZ='https://github.com/fdreserve/bootstrap/releases/download/2/bootstrap.dat'
+BOOTSTRAP_TGZ='https://fdreserve.com/downloads/snapshot.zip'
 COIN_DAEMON="fdreserved"
 COIN_CLI="fdreserve-cli"
 COIN_NAME='FDReserve'
@@ -15,18 +15,25 @@ COIN_NAME='FDReserve'
   echo -e "Updating your System"
 #apt-get update > /dev/null 2>&1
 #apt-get upgrade -y > /dev/null 2>&1
-  echo -e "Download $COIN_NAME update"
-  cd ~/
-  TMP_FOLDER=$(mktemp -d)
-  cd $TMP_FOLDER
-  wget --progress=bar:force $BOOTSTRAP_TGZ > /dev/null 2>&1
-  mv bootstrap.dat /home/$USER/.fdreserve
   sleep 2
   echo -e "Stoping your $COIN_NAME Nodes"
   $COIN_CLI stop > /dev/null 2>&1
   echo -e "Updating $COIN_NAME"
-  rm -rf /home/$USER/.fdreserve/blocks /home/$USER/.fdreserve/chainstate /home/$USER/.fdreserve/db.log /home/$USER/.fdreserve/peers.dat /home/$USER/.fdreserve/debug.log /home/$USER/.fdreserve/fee_estimates.dat /home/$USER/.fdreserve/mncache.dat  
-  cd ~/ >/dev/null
+ sleep 1
+  cd /home/$USER/.fdreserve
+  rm -rf blocks chainstate peers.dat
+  sleep 1
+  echo -e "Downloading BootStrap"
+  wget --progress=bar:force $BOOTSTRAP_TGZ 2>&1 | progressfilt
+  unzip snapshot.zip >/dev/null 2>&1
+  sleep 2
+  cd Snapshot
+  sleep 2
+  mv -f * ../
+  cd ..
+  rm -rf snapshot.zip Snapshot
+  cd ~
+  sleep 2
   rm -rf $TMP_FOLDER >/dev/null 2>&1
   rm update-multi-2-1-4.sh
 
