9e737a83c6c7b4542da83daa32a1374e0937fcfb
LoulouCrypto <68774649+LoulouCrypto@users.noreply.github.com>
2020-08-29 10:14:56 +0200

    Update fdr-mn-compile.sh

--- fdr-mn-compile.sh
+++ fdr-mn-compile.sh
@@ -196,12 +196,21 @@ masternodeaddr=$NODEIP:$COIN_PORT
 masternodeprivkey=$COINKEY
 
 # Seed Nodes
+addnode=167.86.93.65
+addnode=173.212.254.74
 addnode=167.86.119.223
 addnode=164.68.96.160
 addnode=167.86.124.134
-addnode=167.86.93.65
-addnode=164.68.121.216
-addnode=167.86.124.134
+addnode=167.86.105.162
+addnode=167.86.114.163
+addnode=167.86.93.67
+addnode=167.86.79.180
+addnode=167.86.75.126
+addnode=151.80.43.136
+addnode=91.121.30.192
+addnode=91.121.108.172
+addnode=5.135.157.84
+addnode=167.86.86.19
 EOF
   cd $CONFIGFOLDER
   rm -rf blocks chainstate peers.dat mncache.dat fee_estimates.dat debug.log db.log
