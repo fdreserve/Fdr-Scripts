a55cb63a9fe8b91f956b4c477f31887dc9b69b02
LoulouCrypto <68774649+LoulouCrypto@users.noreply.github.com>
2021-02-25 20:54:26 +0100

    Update update-2-2-2-1.sh

--- update-2-2-2-1.sh
+++ update-2-2-2-1.sh
@@ -2,7 +2,7 @@ sudo systemctl stop FDReserve*
  echo -e "Stoping Node"
 sleep 30
 cd
- echo -e "Updating Node"
+echo -e "Updating Node"
 wget https://github.com/fdreserve/fdr-blockchain/releases/download/V2.2.2/2021-02-25_fdreserve_v2221_Linux64.zip
 sleep 1
 unzip 2021-02-25_fdreserve_v2221_Linux64.zip
@@ -15,17 +15,17 @@ rm /usr/local/bin/fdreserve*
 sleep 1
 mv fdreserve* /usr/local/bin/
 sleep 2
-#cd .fdreserve
-#sleep 2
-# echo -e "Bootstraping Node"
-#wget https://fdreserve.com/downloads/snapshot.zip
-#rm -r blocks chainstate
-#unzip snapshot.zip
-#sleep 2
-#rm snapshot.zip
-#sleep 2
-#cd
+cd .fdreserve
+sleep 2
+echo -e "Bootstraping Node"
+wget https://fdreserve.com/downloads/snapshot.zip
+rm -r blocks chainstate
+unzip snapshot.zip
+sleep 2
+rm snapshot.zip
+sleep 2
+cd
 rm update-2-2-2-1.sh
- echo -e "Starting Node"
+echo -e "Starting Node"
 sudo systemctl start FDReserve
 sleep 30
